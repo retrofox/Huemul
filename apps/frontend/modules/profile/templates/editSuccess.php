14478d70bc979351d3e65388084b6bc8df7bb42a
Gula <gerardo.leiva.calderon>
2010-04-08 13:46:44 -0300

    fecha de nacimiento

--- apps/frontend/modules/profile/templates/editSuccess.php
+++ apps/frontend/modules/profile/templates/editSuccess.php
@@ -1,3 +1,4 @@
-<h1>Edit Profile</h1>
+<?php use_helper('I18N') ?>
+<h1><?php echo __('Edit Profile') ?></h1>
 
 <?php include_partial('form', array('form' => $form)) ?>
--- lib/form/doctrine/ProfileForm.class.php
+++ lib/form/doctrine/ProfileForm.class.php
@@ -12,5 +12,16 @@ class ProfileForm extends BaseProfileForm
 {
   public function configure()
   {
+
+    $range  = range(date('Y'), date('Y')-100);
+    $years = array_combine($range,$range);
+
+    $this->widgetSchema['birth_date'] = new sfWidgetFormDate(array(
+      'years' => $years
+    ));
+
+    $this->validatorSchema['birth_date'] = new sfValidatorDate(array(
+      'required' => 'true'
+    ));
   }
 }
