290b9168a00bd48b6937fd0b03281150629a1158
RetroFox <damian.suarez@xifox.net>
2010-04-08 21:17:36 -0300

    Acomodamos un poco el formulario del trámite

--- lib/form/doctrine/CadastralDataForm.class.php
+++ lib/form/doctrine/CadastralDataForm.class.php
@@ -8,13 +8,17 @@
  * @author     Damian Suarez
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class CadastralDataForm extends BaseCadastralDataForm
-{
-  public function configure()
-  {
+class CadastralDataForm extends BaseCadastralDataForm {
+  public function configure() {
     unset(
       $this['created_at'],
       $this['updated_at']
     );
+
+    $this->widgetSchema->setLabels(array(
+      'tipo_numero'  => 'Numero',
+      'partida_nro'  => 'Partida',
+      'uf'  => 'Unidad Funcional',
+    ));
   }
 }
