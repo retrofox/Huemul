4c7ec40974e385f859bf77d90e94aaf9539e87ae
LauraMelo <laura.melo@xifox.net>
2010-04-12 17:02:46 -0300

    modificacion de estilos

--- data/generator/sfDoctrineModule/mooDoo.2/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/mooDoo.2/template/templates/_assets.php
@@ -6,10 +6,12 @@
 [?php use_javascript('<?php echo '/mooDoo.2/js/mootools-1.2.4.4-more.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo '/mooDoo.2/js/admin.generator.class.js' ?>', 'first') ?]
 [?php use_javascript('<?php echo '/mooDoo.2/js/admin.generator.js' ?>', 'first') ?]
-
+<?php
+/*
 [?php use_stylesheet('<?php echo '/css/mooDoo.2/generator.global.css' ?>', 'first') ?]
 [?php use_stylesheet('<?php echo '/css/mooDoo.2/generator.default.css' ?>', 'first') ?]
-
+*/
+?>
 <?php
 /*
 [?php use_stylesheet('<?php echo sfConfig::get('sf_admin_module_web_dir').'/css/global.css' ?>', 'first') ?]
