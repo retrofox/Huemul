87078ebee5f2e161e5f2ee9f2aafd7f7b7d33bb2
LauraMelo <laura.melo@xifox.net>
2010-04-22 21:10:44 -0300

    template de pdf completos

--- apps/backend/modules/sfTCPDF/actions/actions.class.php
+++ apps/backend/modules/sfTCPDF/actions/actions.class.php
@@ -19,202 +19,158 @@ class sfTCPDFActions extends sfActions
   {
     $this->forward('default', 'module');
   }
-public function executeConstancia()
-{
-$config = sfTCPDFPluginConfigHandler::loadConfig();
+  public function executeConstancia()
+  {
+    $config = sfTCPDFPluginConfigHandler::loadConfig();
 
-// create new PDF document
-$pdf = new MyPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    // create new PDF document
+    $pdf = new MyPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-// set default monospaced font
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+    // set default monospaced font
+    $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-//set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+    //set margins
+    $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
-//set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+    //set auto page breaks
+    $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
-//set image scale factor
-$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+    //set image scale factor
+    $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-//set some language-dependent strings
-$pdf->setLanguageArray($l);
+    //set some language-dependent strings
+    $pdf->setLanguageArray($l);
 
-// ---------------------------------------------------------
+    // ---------------------------------------------------------
 
-// add a page
-$pdf->AddPage();
+    // add a page
+    $pdf->AddPage();
 
-// print a line using Cell()
-$pdf->Cell(0, 20, 'CONSTANCIA', 0, 1, 'C');
-$pdf->Cell(0, 0,'La presente constancia habilita al profesional actuante a iniciar el expediente municipal para el', 0, 1, 'L');
-$pdf->Cell(0, 0,'registro del plano de obra, en las condiciones descriptas y según los datos que a continuación', 0, 1, 'L');
-$pdf->Cell(0, 0,'se detallan.', 0, 1, 'L');
-$pdf->Cell(70, 0,'EXPEDIENTE:', 0, 0, 'L');
-$pdf->Cell(0, 0,'EXPEDIENTE', 0, 1, 'L');	
-$pdf->Cell(70, 0,'PROPIETARIO:', 0, 0, 'L');
-$pdf->Cell(0, 0,'PROPIETARIO', 0, 1, 'L');
-$pdf->Cell(70, 0,'TIPO TRAMITE:', 0, 0, 'L');
-$pdf->Cell(0, 0,'TIPO TRAMITE', 0, 1, 'L');
-$pdf->Cell(70, 0,'DOMICILIO DEL INMUEBLE:', 0, 0, 'L');
-$pdf->Cell(0, 0,'DOMICILIO DEL INMUEBLE', 0, 1, 'L');
-$pdf->Cell(70, 0,'BARRIO:', 0, 0, 'L');
-$pdf->Cell(0, 0,'BARRIO', 0, 1, 'L');
-$pdf->Cell(70, 0,'NOMECLATURA CATASTRAL:', 0, 0, 'L');
-$pdf->Cell(0, 0,'NOMECLATURA CATASTRAL', 0, 1, 'L');
-$pdf->Cell(0, 0,'OBSERVACIONES PENDIENTES/DOCUMENTACION A ANEXAR CON EL PLANO:', 0, 1, 'L');
-$pdf->Cell(0, 0,'OBSERVACIONES', 0, 1, 'L');
-$pdf->Ln(30);
-$pdf->Cell(40, 0,'FIRMA DEL PROPIETARIO', 0, 0, 'L');
-$pdf->Cell(0, 0,'FIRMA DEL PROFESIONAL ACTUANTE', 0, 1, 'R');
-$pdf->Ln(60);
-$pdf->Cell(0, 0,'DIRRECCION DE DESARROLLO URBANO Y CATASTRO', 0, 1, 'C');
-$pdf->Cell(0, 0,'MUNICIPALIDAD DE CIPOLLETTI', 0, 1, 'C');
-// ---------------------------------------------------------
+    // print a line using Cell()
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, 'B U', PDF_FONT_SIZE_MAIN+2);
+    $pdf->Cell(0, 20, 'CONSTANCIA', 0, 1, 'C');
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN);
+    $pdf->MultiCell(0, 0,'La presente constancia habilita al profesional actuante a iniciar el expediente municipal para el registro del plano de obra, en las condiciones descriptas y según los datos que a continuación se detallan.', 0, 1, 'L');
+    $pdf->Ln(1);
+    $pdf->Cell(70, 0,'EXPEDIENTE:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'EXPEDIENTE', 0, 1, 'L');
+    $pdf->Cell(70, 0,'PROPIETARIO:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'PROPIETARIO', 0, 1, 'L');
+    $pdf->Cell(70, 0,'TIPO TRAMITE:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'TIPO TRAMITE', 0, 1, 'L');
+    $pdf->Cell(70, 0,'DOMICILIO DEL INMUEBLE:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'DOMICILIO DEL INMUEBLE', 0, 1, 'L');
+    $pdf->Cell(70, 0,'BARRIO:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'BARRIO', 0, 1, 'L');
+    $pdf->Cell(70, 0,'NOMECLATURA CATASTRAL:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'NOMECLATURA CATASTRAL', 0, 1, 'L');
+    $pdf->Ln(3);
+    $pdf->Cell(0, 0,'OBSERVACIONES PENDIENTES/DOCUMENTACION A ANEXAR CON EL PLANO:', 0, 1, 'L');
+    $pdf->MultiCell(0, 0,'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed nulla nibh. Etiam porta fermentum pellentesque. Duis pretium erat vitae massa tristique eu luctus felis mattis. In felis erat, cursus sed suscipit nec, mollis a enim. Sed vulputate urna felis. Praesent vitae malesuada dolor. Praesent consequat bibendum purus ut egestas. In dui elit, gravida vitae vehicula nec, semper at lacus. Vivamus sed lorem at nisl consectetur lacinia ut eu magna. Suspendisse luctus mi ac velit congue id tempor felis pretium. Quisque sed mi vel elit ornare semper nec id massa. In hac habitasse platea dictumst. Vivamus iaculis, lacus eu ullamcorper faucibus, mauris nisi pellentesque augue, eu fringilla ipsum elit vel dolor. In eget faucibus sapien. Praesent orci augue, consequat nec vulputate sed, consequat et sapien. Aenean molestie congue fermentum. In eget ligula tortor, id euismod sem.', 0, 1, 'L');
+    $pdf->Ln(30);
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, 'B', PDF_FONT_SIZE_MAIN);
+    $pdf->Cell(40, 0,'FIRMA DEL PROPIETARIO', 0, 0, 'L');
+    $pdf->Cell(0, 0,'FIRMA DEL PROFESIONAL ACTUANTE', 0, 1, 'R');
+    $pdf->Ln(60);
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN);
+    $pdf->Cell(0, 0,'DIRECCION DE DESARROLLO URBANO Y CATASTRO', 0, 1, 'C');
+    $pdf->Cell(0, 0,'MUNICIPALIDAD DE CIPOLLETTI', 0, 1, 'C');
+    // ---------------------------------------------------------
 
-//Close and output PDF document
-$pdf->Output('constancia.pdf', 'I');
+    //Close and output PDF document
+    $pdf->Output('constancia.pdf', 'I');
+  }
+  
+  
+  public function executePermiso()
+  {
+    $config = sfTCPDFPluginConfigHandler::loadConfig();
 
-//============================================================+
-// END OF FILE
-//============================================================+
+    // create new PDF document
+    $pdf = new MyPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-}
-  /**
- * Hello world test.
- */
-public function executeTest()
-{
-  $config = sfTCPDFPluginConfigHandler::loadConfig();
+    // set default monospaced font
+    $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-  // pdf object
-  $pdf = new MyPDF();
+    //set margins
+    $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
-  // settings
- /* $pdf->SetFont("FreeSerif", "", 12);
-  $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-  $pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-  $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
-  $pdf->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-  $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-  $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-*/
-  // init pdf doc
-  $pdf->AliasNbPages();
-  $pdf->AddPage();
-  $pdf->Cell(80, 10, "Huemul !!! €àèéìòù");
+    //set auto page breaks
+    $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
-  // output
-  $pdf->Output();
+    //set image scale factor
+    $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-  // Stop symfony process
-  throw new sfStopException();
-}
-/**
- * Full test.
- */
-public function executeTest2()
-{
-  $config = sfTCPDFPluginConfigHandler::loadConfig();
-  sfTCPDFPluginConfigHandler::includeLangFile($this->getUser()->getCulture());
+    //set some language-dependent strings
+    $pdf->setLanguageArray($l);
 
-  $doc_title    = "test title";
-  $doc_subject  = "test description";
-  $doc_keywords = "test keywords";
-  $htmlcontent  = '&lt; &euro; €àèéìòù &copy; &gt;<br /><h1>heading 1</h1><h2>heading 2</h2><h3>heading 3</h3><h4>heading 4</h4><h5>heading 5</h5><h6>heading 6</h6>ordered list:<br /><ol><li><b>bold text</b></li><li><i>italic text</i></li><li><u>underlined text</u></li><li><a href="http://www.tecnick.com">link to http://www.tecnick.com</a></li><li>test break<br />second line<br />third line</li><li><font size="+3">font + 3</font></li><li><small>small text</small></li><li>normal <sub>subscript</sub> <sup>superscript</sup></li></ul><hr />table:<br /><table border="1" cellspacing="1" cellpadding="1"><tr><th>#</th><th>A</th><th>B</th></tr><tr><th>1</th><td bgcolor="#cccccc">A1</td><td>B1</td></tr><tr><th>2</th><td>A2 â‚¬ &euro; &#8364; &amp; Ã¨ &egrave; </td><td>B2</td></tr><tr><th>3</th><td>A3</td><td><font color="#FF0000">B3</font></td></tr></table><hr />image:<br /><img src="sfTCPDFPlugin/images/logo_example.png" alt="test alt attribute" width="100" height="100" border="0" />';
+    // ---------------------------------------------------------
 
-  //create new PDF document (document units are set by default to millimeters)
-  $pdf = new sfTCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true);
+    // add a page
+    $pdf->AddPage();
 
-  // set document information
-  $pdf->SetCreator(PDF_CREATOR);
-  $pdf->SetAuthor(PDF_AUTHOR);
-  $pdf->SetTitle($doc_title);
-  $pdf->SetSubject($doc_subject);
-  $pdf->SetKeywords($doc_keywords);
+    // print a line using Cell()
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, 'B U', PDF_FONT_SIZE_MAIN+2);
+    $pdf->Cell(0, 20, 'PERMISO DE CONTRUCCION', 0, 1, 'C');
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN);
+    $pdf->Cell(70, 0,'EXPEDIENTE:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'EXPEDIENTE', 0, 1, 'L');
+    $pdf->Cell(70, 0,'PROPIETARIO:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'PROPIETARIO', 0, 1, 'L');
+    $pdf->Cell(70, 0,'DOMICILIO DEL INMUEBLE:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'DOMICILIO DEL INMUEBLE', 0, 1, 'L');
+    $pdf->Cell(70, 0,'BARRIO:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'BARRIO', 0, 1, 'L');
+    $pdf->Cell(70, 0,'NOMECLATURA CATASTRAL:', 0, 0, 'L');
+    $pdf->Cell(0, 0,'NOMECLATURA CATASTRAL', 0, 1, 'L');
+    $pdf->Ln(3);
+    $pdf->Cell(0, 0,'OBSERVACIONES PENDIENTES/DOCUMENTACION A ANEXAR CON EL PLANO:', 0, 1, 'L');
+    $pdf->MultiCell(0, 0,'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed nulla nibh. Etiam porta fermentum pellentesque.', 0, 1, 'L');
+    $pdf->Ln(10);
+    $pdf->Cell(0, 0,'DOMICILIO Y TELEFONO:', 0, 1, 'L');
+    $pdf->Cell(0, 0,'MATRICULA:', 0, 1, 'L');
+    $pdf->Cell(0, 0,'CALCULO:', 0, 1, 'L');
+    $pdf->Cell(150, 0,'Firma: ', 0, 1, 'R');
+    $pdf->Cell(0, 0,'DOMICILIO Y TELEFONO:', 0, 1, 'L');
+    $pdf->Cell(0, 0,'MATRICULA:', 0, 1, 'L');
+    $pdf->Cell(0, 0,'DIRECTOR DE OBRA:', 0, 1, 'L');
+    $pdf->Cell(150, 0,'Firma: ', 0, 1, 'R');
+    $pdf->Cell(0, 0,'DOMICILIO Y TELEFONO:', 0, 1, 'L');
+    $pdf->Cell(0, 0,'MATRICULA:', 0, 1, 'L');
+    $pdf->Cell(0, 0,'EJECUTOR:', 0, 1, 'L');
+    $pdf->Cell(0, 0,'DOMICILIO Y TELEFONO:', 0, 1, 'L');
+    $pdf->Cell(0, 0,'MATRICULA:', 0, 1, 'L');
+    $pdf->Cell(150, 0,'Firma: ', 0, 1, 'R');
+    $pdf->Ln(20);
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, 'B', PDF_FONT_SIZE_MAIN);
+  	$pdf->Cell(150, 0,'Arq.Roberto A. Bianchi', 0, 1, 'R');
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN);
+    $pdf->Cell(147, 0,'Dep. Obras Privadas', 0, 1, 'R');
+    $pdf->Ln(5);
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, 'B', PDF_FONT_SIZE_MAIN-2);
+    $pdf->Cell(0, 0,'NOTAS', 0, 1, 'L');
+    $pdf->SetFont(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN-2);
+    $pdf->MultiCell(0, 0,'-Es obligación colocar letrero sobre el frente de la obra con los datos que constan en este certificado.-
+-Recuerde mantener el espacio público (vereda y calle) en perfectas condiciones de orden y limpieza. El presente permiso no habilita la ocupación del mismo, en caso de ser necesaria debe tramitarlo por separado. Resguarde la seguridad de los bienes y personas en general y en particular en lo que respecta a los inmuebles linderos, y en la vía pública utilice de bandejas protectoras y demás elementos de seguridad y señalización.-
+-Si necesita deprimir napa prevea el asesoramiento municipal para la evacuación del agua sin volcado a la vía pública.
+-Prevea las condiciones necesarias y obligatorias de seguridad y sanitarias del personal afectado a la obra.
+-Concluida la obra deberá tramitar certificado parcial/final de obra, requisito necesario para la obtención del permiso de conexión de gas. Si surgieran ampliaciones o modificaciones en el transcurso de la ejecución de la misma deberá presentar planos previo a su inicio, caso contrario adjuntar planos conforme a obra para la obtención del certificado.-
+-El presente permiso exime del pago de impuesto al baldío y multas por falta de cerco y vereda durante la vigencia del mismo.-
+                          ', 0, 1, 'L');
+$pdf->SetFont(PDF_FONT_NAME_MAIN, 'b', PDF_FONT_SIZE_MAIN-1);
+    $pdf->MultiCell(0, 0,'Recibió:
+Firma:
+Aclaración:', 0, 1, 'L');
 
-  $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
 
-  //set margins
-  $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 
-  //set auto page breaks
-  $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-  $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-  $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-  $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO); //set image scale factor
+    // ---------------------------------------------------------
 
-  $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-  $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-  //initialize document
-  $pdf->AliasNbPages();
-  $pdf->AddPage();
-
-  // set barcode
-  $pdf->SetBarcode(date("Y-m-d H:i:s", time()));
-
-  // output some HTML code
-  $pdf->writeHTML($htmlcontent , true, 0);
-
-  // output two html columns
-  $first_column_width = 80;
-  $current_y_position = $pdf->getY();
-  $pdf->writeHTMLCell($first_column_width, 0, 0, $current_y_position, "<b>hello</b>", 0, 0, 0);
-  $pdf->writeHTMLCell(0, 0, $first_column_width, $current_y_position, "<i>world</i>", 0, 1, 0);
-
-  // output some content
-  $pdf->Cell(0,10,"TEST Bold-Italic Cell",1,1,'C');
-
-  // output some UTF-8 test content
-  $pdf->AddPage();
-  $pdf->SetFont("FreeSerif", "", 12);
-
-  $utf8text = file_get_contents(K_PATH_CACHE. "utf8test.txt", false); // get utf-8 text form file
-  $pdf->SetFillColor(230, 240, 255, true);
-  $pdf->Write(5,$utf8text, '', 1);
-
-  // remove page header/footer
-  $pdf->setPrintHeader(false);
-  $pdf->setPrintFooter(false);
-
-  // Two HTML columns test
-  $pdf->AddPage();
-  $right_column = "<b>right column</b> right column right column right column right column
-  right column right column right column right column right column right column
-  right column right column right column right column right column right column";
-  $left_column = "<b>left column</b> left column left column left column left column left
-  column left column left column left column left column left column left column
-  left column left column left column left column left column left column left
-  column";
-  $first_column_width = 80;
-  $second_column_width = 80;
-  $column_space = 20;
-  $current_y_position = $pdf->getY();
-  $pdf->writeHTMLCell($first_column_width, 0, 0, 0, $left_column, 1, 0, 0);
-  $pdf->Cell(0);
-  $pdf->writeHTMLCell($second_column_width, 0, $first_column_width+$column_space, $current_y_position, $right_column, 0, 0, 0);
-
-  // add page header/footer
-  $pdf->setPrintHeader(true);
-  $pdf->setPrintFooter(true);
-
-  $pdf->AddPage();
-
-  // Multicell test
-  $pdf->MultiCell(40, 5, "A test multicell line 1\ntest multicell line 2\ntest multicell line 3", 1, 'J', 0, 0);
-  $pdf->MultiCell(40, 5, "B test multicell line 1\ntest multicell line 2\ntest multicell line 3", 1, 'J', 0);
-  $pdf->MultiCell(40, 5, "C test multicell line 1\ntest multicell line 2\ntest multicell line 3", 1, 'J', 0, 0);
-  $pdf->MultiCell(40, 5, "D test multicell line 1\ntest multicell line 2\ntest multicell line 3", 1, 'J', 0, 2);
-  $pdf->MultiCell(40, 5, "F test multicell line 1\ntest multicell line 2\ntest multicell line 3", 1, 'J', 0);
-
-  // Close and output PDF document
-  $pdf->Output();
-
-  // Stop symfony process
-  throw new sfStopException();
-}
+    //Close and output PDF document
+    $pdf->Output('permiso.pdf', 'I');
+  }
 }
