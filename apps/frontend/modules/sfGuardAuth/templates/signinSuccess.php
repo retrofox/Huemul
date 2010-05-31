eb3637aaa66a5a10ffa147c45518880cafc71b8f
Damian <damian.suarez@xifox.net>
2010-05-31 08:38:05 -0300

    Link de descarga de A9CAD.

--- apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -3,8 +3,7 @@
   <table class="orange">
     <caption><?php echo __('Login'); ?></caption>
     <?php echo $form ?>
-  
- <tfoot>
+    <tfoot>
       <tr>
         <td colspan="2">
           <input type="submit" value="Aceptar" />
@@ -13,5 +12,7 @@
     </tfoot>
   </table>
 </form>
-
-
+<hr />
+<h4>A9CAD</h4>
+<p>Si usted necesitad un editor CAD gratuito para diseño y edición 2D, con las herramientas básicas y necesarias que pueda leer archivos .dwg puede utitlizat <a href="http://www.a9tech.com/" title="A9CAD">A9CAD</a>.</p>
+<hr />
