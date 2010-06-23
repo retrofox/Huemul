bcb992cf4eace9e1d695f8cf61dd1f7c698f8cdb
LauraMelo <laura.melo@xifox.net>
2010-06-23 15:26:52 -0300

    correccion de estilos para IE

--- apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -15,4 +15,6 @@
 <hr />
 <h4>A9CAD</h4>
 <p>Si usted necesita un editor CAD gratuito para diseño y edición 2D, con las herramientas básicas y necesarias que pueda leer archivos .dwg puede utitlizar <a href="http://www.a9tech.com/" target="_blank" title="A9CAD">A9CAD</a>.</p>
+<h4>Navegador</h4>
+<p>Recomendamos utilizar <a href="http://www.mozilla.com/firefox/">Firefox</a> o <a href="http://www.google.com/chrome">Chrome</a> para esta aplicación. Esta aplicación no funciona correctamente en Internet Explorer 6 </p>
 <hr />
