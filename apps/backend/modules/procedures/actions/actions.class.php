7082642fdbe63bb1e2825f2410753589e58fa7bb
Gula <gerardo.leiva.calderon>
2010-10-19 10:59:47 -0300

    out submodule

--- apps/backend/modules/procedures/actions/actions.class.php
+++ apps/backend/modules/procedures/actions/actions.class.php
@@ -21,6 +21,34 @@ class proceduresActions extends autoProceduresActions {
     $this->procedure = Doctrine::getTable('Procedure')->find($request->getParameter('id'));
   }
 
+  /**Flitros **/
+  public function executeFilter(sfWebRequest $request)
+  {
+    $this->setPage(1);
+
+    if ($request->hasParameter('_reset'))
+    {
+      $this->setFilters($this->configuration->getFilterDefaults());
+
+      $this->redirect('@procedure');
+    }
+
+  
+    $this->filters = $this->configuration->getFilterForm($this->getFilters());
+    
+    $this->filters->bind($request->getParameter($this->filters->getName()));
+    if ($this->filters->isValid())
+    {
+      $this->setFilters($this->filters->getValues());
+
+      $this->redirect('@procedure');
+    }
+
+    $this->pager = $this->getPager();
+    $this->sort = $this->getSort();
+
+    $this->setTemplate('index');
+  }
   /**
    * action Comprobante
    *
--- lib/filter/doctrine/ProcedureFormFilter.class.php
+++ lib/filter/doctrine/ProcedureFormFilter.class.php
@@ -12,13 +12,28 @@ class ProcedureFormFilter extends BaseProcedureFormFilter
 {
   public function configure()
   {
+    $this->disableLocalCSRFProtection();
+
+    $this->setWidget('pendientes', new sfWidgetFormInputCheckbox());
+    $this->setValidator('pendientes', new sfValidatorString(array('required' => false)));
 
     $this->setWidget('creator', new sfWidgetFormFilterInput(array('with_empty' => false)));
-
     $this->setValidator('creator', new sfValidatorPass(array('required' => false)));
 
     $this->setWidget('state', new sfWidgetFormDoctrineChoice(array('model' => 'RevisionState', 'add_empty' => true)));
     $this->setValidator('state', new sfValidatorDoctrineChoice(array('required' => false, 'model' => 'RevisionState', 'column' => 'id')));
+
+    $this->setWidget('created_at', new sfWidgetFormFilterDate(array(
+            'from_date' => new sfWidgetFormDate(array( 'format'=> '%day%/%month%/%year%')),
+            'to_date' => new sfWidgetFormDate(array( 'format'=> '%day%/%month%/%year%')),
+            'with_empty' => false,
+            'template' => 'desde %from_date%<br />hasta %to_date%')));
+
+    $this->setWidget('updated_at', new sfWidgetFormFilterDate(array(
+            'from_date' => new sfWidgetFormDate(array( 'format'=> '%day%/%month%/%year%')),
+            'to_date' => new sfWidgetFormDate(array( 'format'=> '%day%/%month%/%year%')),
+            'with_empty' => false,
+            'template' => 'desde %from_date%<br />hasta %to_date%')));
   }
 
    public function getFields()
@@ -26,6 +41,7 @@ class ProcedureFormFilter extends BaseProcedureFormFilter
     $fields = parent::getFields();
     $fields['creator'] = 'custom';
     $fields['state'] = 'custom';
+    $fields['pendientes'] = 'custom';
     return $fields;
   }
 
@@ -53,9 +69,31 @@ class ProcedureFormFilter extends BaseProcedureFormFilter
     //SELECT * FROM _procedure p JOIN (SELECT max(number), revision_state_id, procedure_id FROM revision) j ON j.procedure_id = p.id where j.revision_state_id=1
     $text = $value['text'];
     if($text)
-      $query->addSelect('(SELECT revision_state_id,  max(number) FROM revision) j WHERE j.procedure_id = _procedure.id) as rv')->andWhere(
-              '(rv.revision_state_id= ?
+       $query->leftJoin($query->getRootAlias().'.Revisions rv')->andWhere('(
+         rv.revision_state_id LIKE ?
+         AND rv.id = (SELECT MAX(rv2.id) FROM revision rv2 WHERE rv2.procedure_id = '.$query->getRootAlias().'.id )
       )', array("%$text%"));
+    return $query;
+  }
+
+public function addPendientesColumnQuery($query, $field, $value)
+  {
+    $single = sfContext::getInstance();
+    $user = $single->getUser()->getGroups();
+    $sql = "";
+    foreach ($user as $u) $sql .= ' OR "'.$u.'"';
+      
+   /*   echo '<p>$user: <b>'.$sql.'</b></p>';
+    die();*/
+
+
+    $text = $value['text'];
+    if($text){
+      $query->leftJoin($query->getRootAlias().'.Revisions rv')->andWhere('(
+         rv.revision_state_id <> ?
+         AND rv.id = (SELECT MAX(rv2.id) FROM revision rv2 WHERE rv2.procedure_id = '.$query->getRootAlias().'.id )
+      )', array(4));
+    }
     return $query;
   }
 }
